cfaca6d2413eba1f7066fc50e9ce57305a39826b
Eric Lagasca <3447220+rclgs@users.noreply.github.com>
2025-06-11 22:29:21 +0800

    Updated some settings, like adding a colorscheme

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,28 +1,27 @@
 -- lua/settings.lua
 
 -- General settings
-vim.opt.number = true                -- Show line numbers
-vim.opt.relativenumber = true        -- Relative line numbers
+vim.opt.number = true                    -- Show line numbers
+vim.opt.relativenumber = true            -- Relative line numbers
 
 -- Indentation settings
-vim.opt.expandtab = true             -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4               -- Number of spaces for indentation
-vim.opt.tabstop = 4                  -- Number of spaces a tab counts for
-vim.opt.smartindent = true           -- Smart indentation
+vim.opt.expandtab = true                 -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4                   -- Number of spaces for indentation
+vim.opt.tabstop = 4                      -- Number of spaces a tab counts for
+vim.opt.smartindent = true               -- Smart indentation
 
 -- Search settings
-vim.opt.hlsearch = true              -- Highlight search results
-vim.opt.incsearch = true             -- Incremental search
+vim.opt.hlsearch = true                  -- Highlight search results
+vim.opt.incsearch = true                 -- Incremental search
 
 -- Display settings
-vim.opt.wrap = true                  -- Enable line wrapping
-vim.opt.linebreak = true             -- Break lines at word boundaries
-vim.opt.textwidth = 80               -- Set text width to 80 columns
-vim.opt.termguicolors = true         -- Enable true colors
+vim.opt.wrap = true                      -- Enable line wrapping
+vim.opt.textwidth = 80                   -- Set text width to 80 columns
+vim.opt.termguicolors = true             -- Enable true colors
 
 -- Folding settings
-vim.opt.foldmethod = 'syntax'        -- Set fold method to syntax
-vim.opt.foldlevel = 99               -- Open all folds by default
+vim.opt.foldmethod = 'syntax'            -- Set fold method to syntax
+vim.opt.foldlevel = 99                   -- Open all folds by default
 
 -- Colorscheme
-vim.cmd('colorscheme tokyonight')    -- Set the colorscheme to tokyonight
+vim.cmd('colorscheme tokyonight-moon')  -- Set the colorscheme to tokyonight
